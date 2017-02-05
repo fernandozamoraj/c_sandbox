ac0d742ee1ae671bedf35b6821078de4236ffda1
Fernando Zamora <Fernando Zamora>
2017-02-05 00:17:46 -0600

    tutorials for C lang related to command line args

---
+++ command_line_args/main.c
@@ -0,0 +1,21 @@
+#include "stdio.h"
+
+int main(int argc, char* argv[]) {
+
+  if(argc == 1){
+      printf("\nOne argument %s", argv[0]);
+  }
+  else if(argc > 1){
+      for(int i=0;i<argc;i++){
+          printf("\nargc %d is %s", i, argv[i]);
+      }
+  }
+
+  int j = 0;
+  scanf("%d", j);
+  int ch = getc(stdin);
+
+
+  
+  return 0;
+}
