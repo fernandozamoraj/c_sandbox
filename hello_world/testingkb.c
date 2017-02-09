3cab6e49d5dec5899d4538b2a73e89e0923992a6
Fernando Zamora <Fernando Zamora>
2017-02-09 03:26:13 -0600

    pushed latest changes... linked list

---
+++ hello_world/testingkb.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+foo(const char* word){
+    this is a quieter keyboard;
+}
+int main(void){
+    printf("\nhello world);
+    for(int i = 0; i < 20;i++){
+        printf("%d", i);
+    }
+
+    for(int j=0; j < 25; j++){
+        printf("\nHello %d times", j);
+
+    }
+
+    foo("hello");
+}
